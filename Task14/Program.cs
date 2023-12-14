de8358d7795d822fb76c2f7b5191a5a114fee220
unknown <igoshin1922@gmail.com>
2023-12-14 20:28:32 +0500

    added task14

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,89 +1,58 @@
 ﻿using System.ComponentModel.Design;
+using System;
+using System.Collections.Generic;
 
 // Написать программу, которая задает пользователю вопрос (вопрос полагает однозначный ответ, типа да нет).
 // Проверить правильность вопроса. Дать пользователю несколько подсказок и попыток ответить на вопрос.
 // Если он угадал спросить у него имя, и вывести на экран поздравление, которое есть конкатенацией нескольких строк, дважды использовав его имя.
 
-
-List<string> questions = new List<string>() // обьявление списка вопросов
-{"Год рождения Николая Васильевича Гоголя - 1809г?(1 да, 0 нет):",
- "C# это обьектно-ориентированный язык?(1 да, 0 нет):",
- "Первым человеком полетевшим в космос является Гагарин?(1 да, 0 нет):",
- "Земля плоская?(1 да, 0 нет):"
-};
-
-Menu(questions); // вызов функции
-
-void Menu(List<string> question) { 
-    Random rnd = new Random();
-    int RandomQuestion = rnd.Next(0,questions.Count); // генерация случайного числа для выбора вопроса
-    int res;
-    string name;
-    switch (RandomQuestion)
+Random rnd = new Random();
+List<Question> q = new List<Question>();
+q.Add(new Question("Какого цвета небо?  \n1.Голубой\n2.Жёлтый\n3.Зелёный\n4.Красный ", new string[] { "Подсказка: оттенок синего"}, 1));
+q.Add(new Question("Какой формы планета земля?\n1.Куб\n2.Пирамида\n3.Шар\n4.Тарелка", new string[] { "Подсказка: шар" }, 3));
+q.Add(new Question("Какой полюс холоднее?\n1.Северный\n2.Южный", new string[] {"Подсказка: всё не так как кажется" }, 2));
+    while (q.Count > 0)
     {
-        case 0: 
-            Console.WriteLine(questions[0]);
-            res = int.Parse(Console.ReadLine());
-                for(;res < 1;) {
-                    if(res == 0) 
-                    {
-                        Console.WriteLine("Подумайте внимательно :)");
-                        Console.WriteLine("Подсказка: число заканчивается на 09");
-                        Console.WriteLine(questions[0]);
-                        res = int.Parse(Console.ReadLine());
-                    }
-                }
-                Console.WriteLine("Вы правильно ответили на вопрос!!!  Введите Ваше имя латиницей:");
-                name = Console.ReadLine();
-                Console.WriteLine($"\n{name}, от всей души поздравляем Вас!!! Все цветы у Ваших ног, {name}");
-            break;
-        case 1: 
-            Console.WriteLine(questions[1]);
-            res = int.Parse(Console.ReadLine());
-                for(;res < 1;) {
-                    if(res == 0) 
-                    {
-                        Console.WriteLine("Подумайте внимательно :)");
-                        Console.WriteLine("Подсказка: да");
-                        Console.WriteLine(questions[1]);
-                        res = int.Parse(Console.ReadLine());
-                    }
-                }
-                Console.WriteLine("Вы правильно ответили на вопрос!!!  Введите Ваше имя латиницей:");
-                name = Console.ReadLine();
-                Console.WriteLine($"\n{name}, от всей души поздравляем Вас!!! Все цветы у Ваших ног, {name}");
-            break;
-        case 2: 
-            Console.WriteLine(questions[2]);
-            res = int.Parse(Console.ReadLine());
-                for(;res < 1;) {
-                    if(res == 0) 
-                    {
-                        Console.WriteLine("Подумайте внимательно :)");
-                        Console.WriteLine("Подсказка: Его имя - Юрий");
-                        Console.WriteLine(questions[2]);
-                        res = int.Parse(Console.ReadLine());
-                    }
-             }
-                Console.WriteLine("Вы правильно ответили на вопрос!!!  Введите Ваше имя латиницей:");
-                name = Console.ReadLine();
-                Console.WriteLine($"\n{name}, от всей души поздравляем Вас!!! Все цветы у Ваших ног, {name}");
-            break;
-        case 3: 
-            Console.WriteLine(questions[3]);
-            res = int.Parse(Console.ReadLine());
-                for(;res >= 1;) {
-                    if(res == 1) 
-                    {
-                        Console.WriteLine("Подумайте внимательно :)");
-                        Console.WriteLine("Подсказка: Теория плоской земли является вымышленной");
-                        Console.WriteLine(questions[3]);
-                        res = int.Parse(Console.ReadLine());
-                    }
-                }
-                Console.WriteLine("Вы правильно ответили на вопрос!!!  Введите Ваше имя латиницей:");
-                name = Console.ReadLine();
-                Console.WriteLine($"\n{name}, от всей души поздравляем Вас!!! Все цветы у Ваших ног, {name}");
-            break;
+        Question current_question = q[rnd.Next(0, q.Count)];
+        Console.WriteLine(current_question.question);
+        int input = WriteAndGetAnswer(current_question);
+        if (input == current_question.correct_answer)
+        {
+            Console.WriteLine("Верный ответ!");
+        }
+        q.Remove(current_question);
+        Console.WriteLine();
     }
-}
+        
+ 
+int WriteAndGetAnswer(Question q)
+{
+    Console.Write("Введите номер ответа:");
+    int input = int.Parse(Console.ReadLine());
+    if (q.correct_answer == input){
+        return q.correct_answer;
+    }
+    else
+    {
+        while(input != q.correct_answer) {
+        Console.WriteLine($"Неверно! {q.tips[q.correct_answer - q.correct_answer]}");
+        input = int.Parse(Console.ReadLine());
+        }
+        return input;
+    }
+        }
+    
+ 
+    public class Question
+    {
+        public string question;
+        public string[] tips;
+        public int correct_answer;
+ 
+        public Question(string question, string[] tips, int correct_answer)
+        {
+            this.question = question;
+            this.tips = tips;
+            this.correct_answer = correct_answer;
+        }
+    }
